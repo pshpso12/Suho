ddf3ba4e29b4cfc191fef13ca2d2433fff954239
pshpso12 <83266366+pshpso12@users.noreply.github.com>
2024-10-16 19:16:45 +0900

    Loading_Server.cs

---
+++ Loading/Loading_Server.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Steamworks;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+using System;
+using UnityEngine.EventSystems;
+using Insight;
+
+/*동작을 쉽게 보기위해 포트폴리오에서는 서버동작과 클라이언트동작을 합쳐두었습니다.*/
+public class Loading_Server : NetworkBehaviour
+{
+		/*클라이언트로 부터 버전 정보를 받고 동일한지 확인합니다.*/
+		[Command]
+    private void CmdSendGameVersionString(string GVersion)
+    {
+        if(GameVersionString == GVersion)
+        {
+            RpcVersionMatch(1);
+        }
+        else
+        {
+            RpcVersionMatch(2);
+        }
+    }
+    /*클라이언트의 변수가 변경되고 CheckGameVersion()에서 이를 확인합니다.*/
+    [TargetRpc]
+    public void RpcVersionMatch(int isVersionMatch)
+    {
+        GameVersionCheck = isVersionMatch;
+    }
+    /*SteamID를 클라이언트로 부터 받아, 해당 SteamID가 DB에 존재하는지 확인합니다.*/
+    [Command]
+    private void CmdSendSteamData(string steamID, NetworkIdentity ClientIdentity)
+    {
+        LodingServerScript.StartCoroutine(LodingServerScript.CheckSteamID(steamID, ClientIdentity));
+    }
+    /*LoadingServerScript를 통해 벤 유저인 경우 클라이언트는 프로그램을 종료합니다.*/
+    [TargetRpc]
+    public void isBanUser(NetworkConnectionToClient target)
+    {
+        Application.Quit();
+    }
+    /*클라이언트는 */
+    [TargetRpc]
+    public void DataSendToClient(NetworkConnectionToClient target, SteamLobby_Server.ServerResponse response)
+    {
+        ClientDataManager.Instance.UpdateUserDetails(response);
+        isUserDataUpdated_1 = true;
+    }
+}
