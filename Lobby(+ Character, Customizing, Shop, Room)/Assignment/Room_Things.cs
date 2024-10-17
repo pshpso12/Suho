3cb3bd7cc30e5831b0a9b6b1c53ca7512b5a9671
pshpso12 <83266366+pshpso12@users.noreply.github.com>
2024-10-17 20:13:28 +0900

    Room_Things.cs

---
+++ Lobby(+ Character, Customizing, Shop, Room)/Assignment/Room_Things.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Loading_Things : MonoBehaviour
+{
+    public GameObject RoomchatUI;
+    public TMP_InputField RoomchatInputField;
+    public TMP_Text RoomInputFieldText;
+}
