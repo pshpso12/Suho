37911875b66ff5b11ae2029d646971e7a59fc7f9
pshpso12 <83266366+pshpso12@users.noreply.github.com>
2024-10-17 20:59:58 +0900

    CharBase.cs

--- Lobby(+ Character, Customizing, Shop, Room)/CharBase.cs
+++ Lobby(+ Character, Customizing, Shop, Room)/CharBase.cs
@@ -64,6 +64,7 @@ public class CharBase : MonoBehaviour
     
     void Start()
     {
+        /*초기 캐릭터 정보를 메인캐릭터 정보로 설정*/
         int mainCharacterId;
 
         if (int.TryParse(ClientDataManager.Instance.UserDetails.MainCharacterID, out mainCharacterId))
@@ -74,12 +75,15 @@ public class CharBase : MonoBehaviour
         }
         else
         {
-            // Handle the case where the string cannot be converted to an integer
             Debug.LogError("MainCharacterID is not a valid integer");
         }
+        /*캐릭처창에서의 캐시, 인게임재화 표기*/
         Main_cashpoint.text = $"{ClientDataManager.Instance.UserDetails.cashpoint.ToString("N0")}";
         Main_basepoint.text = $"{ClientDataManager.Instance.UserDetails.basepoint.ToString("N0")}";
 
+        /*캐릭터의 스킬 이미지 네개를 표기하고 PointerEenter 시 각 스킬에 맞는 정보를 툴팁에 표기하기 위해 준비
+         마우스가 스킬 이미지를 벗어났을 경우 툴팁 숨기기
+         (툴팁을 열었을 경우 마우스가 스킬 이미지를 벗어나 툴팁으로 이동하여 툴팁이 꺼지는 현상을 Pivot(1.01,1.01)로 수정해서 해결)*/
         Image[] skillImages = { Skill_1, Skill_2, Skill_3, Skill_4 };
         for (int i = 0; i < skillImages.Length; i++)
         {
@@ -102,11 +106,12 @@ public class CharBase : MonoBehaviour
 
     void Update()
     {
+        /*툴팁의 위치를 마우스의 위치에 따라 움직이게 하기 위함*/
         if (tooltipPanel != null && tooltipPanel.activeSelf)
         {
             Vector2 localPoint;
             RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                (tooltipPanel.transform.parent as RectTransform), // Assuming the tooltipPanel is placed within a Canvas
+                (tooltipPanel.transform.parent as RectTransform),
                 Input.mousePosition, 
                 null, // Assuming the Canvas is Screen Space - Overlay
                 out localPoint
@@ -120,6 +125,7 @@ public class CharBase : MonoBehaviour
         }
     }
 
+    /*툴팁 표기 방식*/
     void ShowSkillTooltip(int itemC)
     {
         currentlySelectedSkillIndex = itemC;
@@ -159,14 +165,16 @@ public class CharBase : MonoBehaviour
         tooltipDescriptionText.text = skillDescription;
 
         tooltipPanel.SetActive(true);
+        /*툴팁의 크기를 설명의 길이에 따라 늘어나고 줄어들게 할 수 있기 위함*/
         LayoutRebuilder.ForceRebuildLayoutImmediate(tooltipPanel.GetComponent<RectTransform>());
     }
+    /*툴팁을 숨기고 currentlySelectedSkillIndex를 null로 변경하여 Update()에서 툴팁 갱신안하게 함*/
     void HideSkillTooltip()
     {
         tooltipPanel.SetActive(false);
         currentlySelectedSkillIndex = null;
     }
-
+    /*현재 선택한 캐릭터를 selection으로 표시해주는데 이를 전부 숨김*/
     public void DeactiveImage()
     {
         foreach(GameObject cha_obj in cha_imgs)
@@ -182,14 +190,17 @@ public class CharBase : MonoBehaviour
             }
         }
     }
+    /*이미지 표기, 선택한 캐릭터 표시*/
     public void ActiveImags(int index)
     {
         DeactiveImage();
         if(index >= 0 && index < cha_imgs.Length)
         {
+            /*cha_imgs[index]로 선택한 캐릭터의 전체 이미지를 활성화*/
             cha_imgs[index].SetActive(true);
             Index_Send = index;
 
+            /*선택한 캐릭터 얼굴 이미지에 선택함을 표시*/
             Transform selection = cha_buttons[index].transform.Find("Selection");
             if (selection != null)
             {
@@ -198,11 +209,13 @@ public class CharBase : MonoBehaviour
             SetCharacterData(index);
         }
     }
+    /*캐릭터 별 정보를 표기*/
     private void SetCharacterData(int index)
     {
         if (index >= 0 && index < characterDataArray.Length)
         {
             CharInfoCharacterData data = characterDataArray[index];
+            /*특성에 따라 색상을 달리하여 표기*/
             ChaFam.text = data.family;
             if (data.family == "Dark")
             {
@@ -212,17 +225,20 @@ public class CharBase : MonoBehaviour
             {
                 ChaFam.color = new Color32(255, 240, 146, 255); // #FFF092
             }
+            /*캐릭터 이름, 대사, 능력치 표기*/
             ChaName.text = data.name;
             ChaExplan.text = data.explanation;
             ChaAPower.text = data.attackPower;
             ChaASpeed.text = data.attackSpeed;
             ChaARange.text = data.attackRange;
 
+            /*스킬 이미지 표기*/
             Skill_1.sprite = data.skill1;
             Skill_2.sprite = data.skill2;
             Skill_3.sprite = data.skill3;
             Skill_4.sprite = data.skill4;
 
+            /*캐릭터를 선택 시 해당 캐릭터의 메인 대사 사운드 재생*/
             InGameSoundManager.Instance?.CharCharacterSound(data.CharacterAudio);
         }
         else
